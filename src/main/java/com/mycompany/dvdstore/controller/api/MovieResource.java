85dc3cfe893ff53f17ee378feaa5feb4103c7569
diallo <souleymane.diallo@etudiant-univ-rennes1.fr>
2022-04-10 17:36:46 +0200

    Section 7/Exercice 31 : DVDStore : Exploiter Spring Data JDBC

--- src/main/java/com/mycompany/dvdstore/controller/api/MovieResource.java
+++ src/main/java/com/mycompany/dvdstore/controller/api/MovieResource.java
@@ -3,15 +3,14 @@ package com.mycompany.dvdstore.controller.api;
 import com.mycompany.dvdstore.entity.Movie;
 import com.mycompany.dvdstore.service.MovieServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/movie")
 public class MovieResource {
+
     @Autowired
     private MovieServiceInterface movieService;
 
@@ -23,23 +22,19 @@ public class MovieResource {
         this.movieService = movieService;
     }
 
-
-    @PostMapping
-    public Movie add(@RequestBody Movie movie) {
-        return movieService.registerMovie(movie);
-
+    @GetMapping
+    public Iterable<Movie> list(){
+        return movieService.getMovieList();
     }
-
 
     @GetMapping("/{id}")
     public Movie get(@PathVariable("id") long id){
         return movieService.getMovieById(id);
     }
 
-    @GetMapping
-    public List<Movie> list(){
-        return movieService.getMovieList();
-
-    }
+    @PostMapping
+    public Movie add(@RequestBody Movie movie){
+        return movieService.registerMovie(movie);
     }
 
+}
